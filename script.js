c60e03b5c49d81f2a468890e96bd9ed09ae88146
Alikhan <chort445@gmail.com>
2024-12-10 23:43:41 +0300

    hw

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('h1').textContent = "Welcome to home";
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs[0].textContent = "Adress: ffaaff");
+console.log(paragraphs[1].textContent = "Pat: Dog");
+console.log(paragraphs[2].textContent = "Family");
+
+const click = document.querySelector('#nameButton').textContent = "Click me";
